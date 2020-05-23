da2363ac24db5a158f1f0840df2cf70086d6d9f9
Mekon18 <maksim.grummo@mail.ru>
2020-05-23 13:31:23 +0300

    init

---
+++ PPO_Kursach/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PPO_Kursach.MvcApplication" Language="C#" %>
---
+++ PPO_Kursach/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PPO_Kursach.Startup))]
+namespace PPO_Kursach
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
