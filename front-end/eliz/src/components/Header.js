b42de81f7cec3563deb5c690b810865ded5e1694
Matvey <matvey2k@gmail.com>
2020-05-25 02:16:01 +0300

    added navbar component

---
+++ front-end/eliz/src/components/Header.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Route } from 'react-router';
+
+
+
+export default class Header extends Component {
+    render() {
+        return (
+            <>
+                <Navbar fixed="top" collapseOnSelect expand="md" bg="dark" variant="dark">
+                    <Container>
+                        <Navbar.Brand>
+                            <img height="30" width="30" className="d-inline-block align-top" />
+                            Элизабет
+                        </Navbar.Brand>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav className="mr-auto">
+                                <Nav.Link href="/">Главная</Nav.Link>
+                                <Nav.Link href="/services">Услуги</Nav.Link>
+                                <Nav.Link href="/team">Врачи</Nav.Link>
+                                <Nav.Link href="/about">О нас</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+                <Router>
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                        <Route exact path="/services" component={Services}/>
+                        <Route exact path="/team" component={Team}/>
+                        <Route exact path="/about" component={About}/>
+                    </Switch>
+                </Router>
+            </>
+        )
+    }
+}
