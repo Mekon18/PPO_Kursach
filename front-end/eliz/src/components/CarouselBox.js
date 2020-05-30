3215eeee3d1861413397dc3b1535ea17c335bb4e
Matvey <matvey2k@gmail.com>
2020-05-30 21:04:05 +0300

    Исправлены импорты

--- front-end/eliz/src/components/CarouselBox.js
+++ front-end/eliz/src/components/CarouselBox.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Carousel from 'react-bootstrap/Carousel'
+import Image from 'react-bootstrap/Image'
 import doctor1 from '../assets/doctor1.jpg'
 import doctor2 from '../assets/doctor2.jpg'
 import doctor3 from '../assets/doctor3.jpg'
@@ -7,23 +8,23 @@ import doctor3 from '../assets/doctor3.jpg'
 export default class CarouselBox extends Component {
     render() {
         return (
-            <Carousel>
+            <Carousel className="mb-4">
                 <Carousel.Item>
-                    <img className="d-block w-100" src={doctor1} alt="доктор1" />
+                    <Image className="d-block w-100" src={doctor1} alt="доктор1" />
                     <Carousel.Caption>
                         <h3>Заголовок1</h3>
                         <p>текст для картинки1</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img className="d-block w-100" src={doctor2} alt="доктор2" />
+                    <Image className="d-block w-100" src={doctor2} alt="доктор2" />
                     <Carousel.Caption>
                         <h3>Заголовок2</h3>
                         <p>текст для картинки2</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img className="d-block w-100" src={doctor3} alt="доктор3" />
+                    <Image className="d-block w-100" src={doctor3} alt="доктор3" />
                     <Carousel.Caption>
                         <h3>Заголовок2</h3>
                         <p>текст для картинки2</p>
