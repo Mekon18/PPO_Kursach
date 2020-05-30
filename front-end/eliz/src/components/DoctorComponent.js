e5b74f7f134b19be94717df68179eb1f244c3261
Matvey <matvey2k@gmail.com>
2020-05-30 23:09:50 +0300

    Обновлён компонент докторов

--- front-end/eliz/src/components/DoctorComponent.js
+++ front-end/eliz/src/components/DoctorComponent.js
@@ -1,35 +1,26 @@
 import React, { Component } from 'react';
 import CardComponent from '../components/Card'
 import { CardDeck } from 'react-bootstrap'
+import axios from 'axios';
 
 class Doctors extends Component {
 
     state = {
-        result: {},
+        people: []
     }
 
     componentDidMount() {
-        fetch("https://localhost:44391/Home/GetDoctors")
-            .then(res => res.json())
-            .then(result => this.SetDoctors(result))
-            .catch(error => error);
-    }
-
-    setDoctors = result => {
-        this.setState({ result });
+        axios.get(`https://localhost:44391/Home/GetDoctors`)
+            .then(res => {
+                const people = res.data;
+                this.setState({ people });
+            })
     }
 
     render() {
-        const { result } = this.state
-        const { hits = [] } = result
-        console.log(result)
         return (
             <CardDeck>
-                {hits.map(({ Name, Speciality }) =>
-                    <CardComponent
-                        title={Name}
-                        text={Speciality}
-                    />)}
+                {this.state.people.map(person => <CardComponent title={person.Nitle} text={person.Speciality} />)}
             </CardDeck>
         );
     }
