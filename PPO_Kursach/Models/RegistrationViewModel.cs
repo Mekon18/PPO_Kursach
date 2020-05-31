065d44bd201a5d143eb692bf8ed068a025bf5e76
Matvey <matvey2k@gmail.com>
2020-05-31 20:41:27 +0300

    Merge branch 'master' of https://github.com/Mekon18/PPO_Kursach Странный коммит

---
+++ PPO_Kursach/Models/RegistrationViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PPO_Kursach.Models
+{
+	public class RegistrationViewModel
+	{
+		public int DoctorId { get; set; }
+		public int ServiceId { get; set; }
+		public string UserName { get; set; }
+	
+		public DateTime Date { get; set; }
+		public TimeSpan time { get; set; }
+	}
+}
