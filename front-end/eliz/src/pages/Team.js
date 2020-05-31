506c1745706f923747d0870ab962f1463eedcb38
Matvey <matvey2k@gmail.com>
2020-05-31 18:02:30 +0300

    Обновлена страница отделений

--- front-end/eliz/src/pages/Team.js
+++ front-end/eliz/src/pages/Team.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import Doctors from '../components/DoctorComponent'
+import { Container, Tab, Row, Col, Nav } from 'react-bootstrap'
 
 export default class Team extends Component {
     render() {
         return (
-            <div>
-                <Doctors/>
-            </div>
+            <Container>
+                <Tab.Container id="tabs" defaultActiveKey="first">
+                    <Row>
+                        <Col sm={3}>
+                            <Nav variant="pills" className="flex-column mt-2">
+                                <Nav.Item>
+                                    <Nav.Link eventKey="first">Какое-то отделение</Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                        </Col>
+                    </Row>
+                    <Col sm={9}>
+                        <Tab.Content>
+                            <Tab.Pane eventKey="first">
+                                <Doctors />
+                            </Tab.Pane>
+                        </Tab.Content>
+                    </Col>
+                </Tab.Container>
+            </Container>
         )
     }
 }
