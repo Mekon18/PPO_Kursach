ad8e7ec4c28d67be5dc66ae905c65e95050af286
Matvey <matvey2k@gmail.com>
2020-06-01 20:25:25 +0300

    Обновлён компонент записи на приём

--- front-end/eliz/src/components/AppointForm.js
+++ front-end/eliz/src/components/AppointForm.js
@@ -9,30 +9,51 @@ class AppointmentForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            inputText: '',
+            name: undefined,
             people: [],
             departments: [],
-            selectedDepartment: '',
-            selectedDoctor: '',
-            selectedDay: ''
+            time: [],
+            selectedDepartment: undefined,
+            selectedDoctor: undefined,
+            selectedDay: undefined,
+            selectedTime: undefined
         }
         this.selectChangeHandler = this.selectChangeHandler.bind(this);
+        this.selectDoctorChangeHandler = this.selectDoctorChangeHandler.bind(this);
+        this.handeInputChange = this.handeInputChange.bind(this);
         this.handleDayClick = this.handleDayClick.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.selectTimeChangeHandler = this.selectTimeChangeHandler.bind(this);
     }
 
     handleDayClick(day, { selected }) {
         if (selected) {
-          // Unselect the day if already selected
-          this.setState({ selectedDay: undefined });
-          return;
+            this.setState({ selectedDay: undefined });
+            return;
         }
         this.setState({ selectedDay: day });
-      }
+        if (this.state.selectedDoctor !== undefined && this.state.selectedDay !== undefined) {
+            axios.get('https://jsonplaceholder.typicode.com/todos')
+                .then(res => {
+                    const times = res.data;
+                    this.setState({ time: times });
+                    this.setState({ selectedTime: times[0].id })
+                })
+        }
+    }
 
     handeInputChange = ({ target: { value } }) => {
         this.setState({
-            inputText: value,
+            inputText: value
         })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log(this.state.name);
+        console.log(this.state.selectedDay.toLocaleDateString().split('.').join('-'));
+        console.log(this.state.selectedDoctor);
+        //axios.post(`${axios.defaults.baseURL}/people`, { user });
     }
 
     componentDidMount() {
@@ -44,21 +65,30 @@ class AppointmentForm extends Component {
     }
 
     selectChangeHandler(event) {
+        this.setState({ selectedDepartment: event.target.value });
+        console.log(this.state.selectedDepartment);
         axios.get(`https://jsonplaceholder.typicode.com/todos`)
             .then(res => {
                 const people = res.data;
                 this.setState({ people: people });
             })
-        this.setState({ selectedDepartment: event.target.value });
-        console.log(this.state.selectedDepartment);
+    }
+
+    selectDoctorChangeHandler(event) {
+        this.setState({ selectedDoctor: event.target.value });
+        console.log(this.state.selectedDoctor);
+    }
+
+    selectTimeChangeHandler(event) {
+        this.setState({ selectedTime: event.target.value });
     }
 
     render() {
         return (
-            <Form>
+            <Form onSubmit={this.handleSubmit}>
                 <Form.Row>
                     <Form.Group as={Col} controlId="formGridName">
-                        <Form.Control placeholder="ФИО" name="name" value={this.state.inputText} onChange={this.handeInputChange} />
+                        <Form.Control placeholder="ФИО" name="name" value={this.state.name} onChange={this.handeInputChange} />
                     </Form.Group>
                 </Form.Row>
 
@@ -69,8 +99,8 @@ class AppointmentForm extends Component {
                         </Form.Control>
                     </Form.Group>
                     <Form.Group as={Col} controlId="formGridState">
-                        <Form.Control as="select" value={this.state.selectedDoctor}>
-                            {this.state.people.map(person => <option value={person.id}>{person.userId}</option>)}
+                        <Form.Control as="select" value={this.state.selectedDoctor} onChange={this.selectDoctorChangeHandler}>
+                            {this.state.people.map(person => <option value={person.id}>{person.title}</option>)}
                         </Form.Control>
                     </Form.Group>
                 </Form.Row>
@@ -79,7 +109,11 @@ class AppointmentForm extends Component {
                     <Form.Group as={Col} controlId="formGridState">
                         <DayPicker onDayClick={this.handleDayClick} selectedDays={this.state.selectedDay} />
                     </Form.Group>
-
+                    <Form.Group as={Col} controlId="formGridState">
+                        <Form.Control as="select" value={this.state.selectedTime} onChange={this.selectTimeChangeHandler}>
+                            {this.state.time.map(time => <option value={time.id}>{time.id}</option>)}
+                        </Form.Control>
+                    </Form.Group>
                 </Form.Row>
 
                 <Button variant="primary" type="submit">
