52baed09216e1c59e3f7ef85b5fb08d8de4c7dce
Matvey <matvey2k@gmail.com>
2020-06-01 17:09:49 +0300

    Добавлен компонент DayPicker

--- front-end/eliz/src/components/AppointForm.js
+++ front-end/eliz/src/components/AppointForm.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
 import { Container, Form, Col, Button } from 'react-bootstrap'
 import axios from 'axios'
+import DayPicker from 'react-day-picker'
+import 'react-day-picker/lib/style.css';
+
 
 class AppointmentForm extends Component {
-    state = {
-        inputText: '',
-        departments: []
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputText: '',
+            people: [],
+            departments: [],
+            selectedDepartment: '',
+            selectedDoctor: '',
+            selectedDay: ''
+        }
+        this.selectChangeHandler = this.selectChangeHandler.bind(this);
+        this.handleDayClick = this.handleDayClick.bind(this);
     }
+
+    handleDayClick(day, { selected }) {
+        if (selected) {
+          // Unselect the day if already selected
+          this.setState({ selectedDay: undefined });
+          return;
+        }
+        this.setState({ selectedDay: day });
+      }
 
     handeInputChange = ({ target: { value } }) => {
         this.setState({
@@ -15,58 +36,50 @@ class AppointmentForm extends Component {
     }
 
     componentDidMount() {
-        axios.get(`https://localhost:44391/Home/GetDepartmens`)
+        axios.get(`https://jsonplaceholder.typicode.com/todos`)
             .then(res => {
                 const departments = res.data;
-                this.setState({ departments });
+                this.setState({ departments: departments });
             })
     }
 
-    selectChangeHandeler() {
-        axios.get(`https://localhost:44391/Home/GetDoctors?` + this.props.departmentId)
+    selectChangeHandler(event) {
+        axios.get(`https://jsonplaceholder.typicode.com/todos`)
             .then(res => {
                 const people = res.data;
-                this.setState({ people });
+                this.setState({ people: people });
             })
+        this.setState({ selectedDepartment: event.target.value });
+        console.log(this.state.selectedDepartment);
     }
 
     render() {
-        const {inputText} = this.state;
         return (
             <Form>
                 <Form.Row>
                     <Form.Group as={Col} controlId="formGridName">
-                        <Form.Control placeholder="ФИО" name="name" value={inputText} onChange={this.handeInputChange} />
+                        <Form.Control placeholder="ФИО" name="name" value={this.state.inputText} onChange={this.handeInputChange} />
                     </Form.Group>
                 </Form.Row>
 
                 <Form.Row>
                     <Form.Group as={Col} controlId="formGridState">
-                        <Form.Control as="select" value="отделение">
-                            {this.state.departments.map(department => <option value={department.id}>{department.title}</option>)}
+                        <Form.Control as="select" value={this.state.selectedDepartment} onChange={this.selectChangeHandler}>
+                            {this.state.departments.map(department => <option value={department.id}>{department.id}</option>)}
                         </Form.Control>
                     </Form.Group>
                     <Form.Group as={Col} controlId="formGridState">
-                        <Form.Control as="select" value="доктор">
-                            <option>Доктор</option>
-                            <option>...</option>
+                        <Form.Control as="select" value={this.state.selectedDoctor}>
+                            {this.state.people.map(person => <option value={person.id}>{person.userId}</option>)}
                         </Form.Control>
                     </Form.Group>
                 </Form.Row>
 
                 <Form.Row>
                     <Form.Group as={Col} controlId="formGridState">
-                        <Form.Control as="select" value="отделение">
-                            <option>День</option>
-                            <option>...</option>
-                        </Form.Control>
+                        <DayPicker onDayClick={this.handleDayClick} selectedDays={this.state.selectedDay} />
                     </Form.Group>
-                    <Form.Group as={Col} controlId="formGridState">
-                        <Form.Control as="select" value="отделение">
-                            <option>Месяц</option>
-                            <option>...</option>
-                        </Form.Control>
-                    </Form.Group>
+
                 </Form.Row>
 
                 <Button variant="primary" type="submit">
