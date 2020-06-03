67bcaf3f3fc4719e41b789ab29027f68a87a8963
Matvey <matvey2k@gmail.com>
2020-06-03 21:39:02 +0300

    Добавлен импорт Col

--- front-end/eliz/src/components/DepartmensAndDoctors.js
+++ front-end/eliz/src/components/DepartmensAndDoctors.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row } from 'react-bootstrap'
+import { Row, Col } from 'react-bootstrap'
 import axios from 'axios';
 import Doctors from '../components/DoctorComponent'
 
