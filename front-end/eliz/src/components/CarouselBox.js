b0e8d16b72e15540aaaa4090861044e079a323a1
Matvey <matvey2k@gmail.com>
2020-06-03 04:07:56 +0300

    Изменён слайдер

--- front-end/eliz/src/components/CarouselBox.js
+++ front-end/eliz/src/components/CarouselBox.js
@@ -8,29 +8,45 @@ import doctor3 from '../assets/doctor3.jpg'
 export default class CarouselBox extends Component {
     render() {
         return (
-            <Carousel className="mb-4">
-                <Carousel.Item>
-                    <Image className="d-block w-100" src={doctor1} alt="доктор1" />
-                    <Carousel.Caption>
-                        <h3>Заголовок1</h3>
-                        <p>текст для картинки1</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <Image className="d-block w-100" src={doctor2} alt="доктор2" />
-                    <Carousel.Caption>
-                        <h3>Заголовок2</h3>
-                        <p>текст для картинки2</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <Image className="d-block w-100" src={doctor3} alt="доктор3" />
-                    <Carousel.Caption>
-                        <h3>Заголовок2</h3>
-                        <p>текст для картинки2</p>
-                    </Carousel.Caption>
-                </Carousel.Item> 
-            </Carousel>
-        )
+            <>
+                <Carousel className="mt-5 mb-4">
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={doctor3}
+                            alt="First slide"
+                        />
+                        <Carousel.Caption>
+                            <h1>First slide label</h1>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={doctor2}
+                            alt="Third slide"
+                        />
+
+                        <Carousel.Caption>
+                            <h1>Second slide label</h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={doctor1}
+                            alt="Third slide"
+                        />
+
+                        <Carousel.Caption>
+                            <h1>Third slide label</h1>
+                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel >
+            </>
+        );
     }
 }
