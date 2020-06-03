efae94519a1dba0c0d881ecedf4a6b470fcc1e48
Matvey <matvey2k@gmail.com>
2020-06-03 04:12:33 +0300

    Добавлен футер

--- front-end/eliz/src/pages/Appointment.js
+++ front-end/eliz/src/pages/Appointment.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import { Container, Form, Col, Button } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
 import AppointForm from '../components/AppointForm'
+import Footer from '../components/Footer'
 
 export default class About extends Component {
     render() {
         return (
             <Container>
-                <h2 className="text-center m-4">На этой странице вы можете записаться на приём к врачу</h2>
+                <h2 className="text-center mt-5 mb-2">На этой странице вы можете записаться на приём к врачу</h2>
                 <AppointForm></AppointForm>
+                <Footer/>
             </Container>
         )
     }
