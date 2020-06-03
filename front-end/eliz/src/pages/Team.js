b3d78e70a85271b51826d2962d85598bca2dd6f6
Matvey <matvey2k@gmail.com>
2020-06-03 21:11:40 +0300

    Обновлена станица врачей

--- front-end/eliz/src/pages/Team.js
+++ front-end/eliz/src/pages/Team.js
@@ -1,29 +1,12 @@
 import React, { Component } from 'react';
-import Doctors from '../components/DoctorComponent'
-import { Container, Tab, Row, Col, Nav } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
+import DepartmensAndDoctors from '../components/DepartmensAndDoctors'
 
 export default class Team extends Component {
     render() {
         return (
             <Container>
-                <Tab.Container id="tabs" defaultActiveKey="first">
-                    <Row>
-                        <Col sm={3}>
-                            <Nav variant="pills" className="flex-column mt-2">
-                                <Nav.Item>
-                                    <Nav.Link eventKey="first">Какое-то отделение</Nav.Link>
-                                </Nav.Item>
-                            </Nav>
-                        </Col>
-                    </Row>
-                    <Col sm={9}>
-                        <Tab.Content>
-                            <Tab.Pane eventKey="first">
-                                <Doctors departmentId={1}/>
-                            </Tab.Pane>
-                        </Tab.Content>
-                    </Col>
-                </Tab.Container>
+                <DepartmensAndDoctors />
             </Container>
         )
     }
