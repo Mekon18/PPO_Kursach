3694a4286bd049ec898d0f7082053cf426592925
Matvey <matvey2k@gmail.com>
2020-06-03 04:06:51 +0300

    Обновлён дизайн главной страницы

--- front-end/eliz/src/pages/Home.js
+++ front-end/eliz/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Carousel from '../components/CarouselBox'
-import { Container, CardDeck, Col, Row } from 'react-bootstrap';
+import { Container, CardDeck, Col, Row, Jumbotron, Button, Modal } from 'react-bootstrap';
 import CardComponent from '../components/Card'
-import Paragraph from '../components/Paragraph'
+import Services from '../components/Services'
+import ModalAppoint from '../components/ModalAppoint'
 
+import Footer from '../components/Footer'
 import best from '../assets/best.jpg'
 import eq from '../assets/equipment.jpg'
 import practises from '../assets/practises.jpg'
@@ -13,37 +15,36 @@ export default class Home extends Component {
     render() {
         return (
             <>
-                <Carousel/>
+                <Carousel />
                 <Container>
                     <Row>
                         <Col>
-                            <Paragraph
-                                title="Что мы из себя представляем"
-                                text="Медицинский центр «Элизабет» - это сочетание истории, 
-                        передовых технологий в медицине, новейшего оборудования, высококвалифицированных 
-                        медицинских работников и комфорта. Так, «Элизабет» представляет собой два филиала, 
-                        расположенных в центральной исторической части города Могилева по переулку 
-                        Тани Карпинской, д.10а и переулку Коммунистическому, д.7."
-                            />
-                            <Paragraph
-                                title="Вы почувствуюете себя как дома"
-                                text="Интерьер фасадов зданий и их помещений 
-                        стирают привычное восприятие поликлиник и больниц, 
-                        создают комфорт, уют и спокойствие. Для удобства клиентов 
-                        филиалы обустроены автомобильными парковками."
-                            />
-                            <Paragraph
-                                title="Мы постоянно совершенствуемся"
-                                text="Наши специалисты постоянно развиваются, 
-                        повышают свой профессиональный уровень не только в Республике Беларусь, 
-                        но и за рубежом. Сотрудники «Элизабет» регулярно участвуют в международных 
-                        научных конференциях и съездах. Это позволяет нашим врачам получить навыки и 
-                        технологии, которые успешно и эффективно применяются на современном высокотехнологичном 
-                        оборудовании, установленном в центре."
-                            />
+                            <h2 className="mb-4">Коротко о нас</h2>
                         </Col>
                     </Row>
-                    <h2 className="text-center m-4">Почему стоит выбрать нас?</h2>
+                    <Row>
+                        <Col sm={6}>
+                            <p>
+                                Медицинский центр «Элизабет» - это сочетание истории, передовых технологий в медицине, новейшего оборудования, высококвалифицированных медицинских работников и комфорта. Так, «Элизабет» представляет собой два филиала, расположенных в центральной исторической части города Могилева по переулку Тани Карпинской, д.10а и переулку Коммунистическому, д.7.
+                            </p>
+                        </Col>
+                        <Col>
+                            <p>
+                                Наши специалисты постоянно развиваются, повышают свой профессиональный уровень не только в Республике Беларусь, но и за рубежом. Сотрудники «Элизабет» регулярно участвуют в международных научных конференциях и съездах. Это позволяет нашим врачам получить навыки и технологии, которые успешно и эффективно применяются на современном высокотехнологичном оборудовании, установленном в центре.
+                            </p>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <h2 className="mb-4">Направления</h2>
+                        </Col>
+                    </Row>
+                    <Services />
+                    <Row className="mt-4">
+                        <Col>
+                            <h2 className="mb-4">Почему стоит выбрать нас?</h2>
+                        </Col>
+                    </Row>
                     <CardDeck className="m-4">
                         <CardComponent
                             image={practises}
@@ -61,7 +62,17 @@ export default class Home extends Component {
                             text="Нужно как-то закончить эту пару"
                         />
                     </CardDeck>
-
+                    <Row>
+                        <Col>
+                            <h2 className="mb-4 text-center">Записыватесь на приём прямо сейчас</h2>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col className="text-center mb-5">
+                            <ModalAppoint />
+                        </Col>
+                    </Row>
+                    <Footer />
                 </Container>
             </>
         )
