c56201667995a100c9a6f2714d9007c7caf725cd
Matvey <matvey2k@gmail.com>
2020-06-03 21:11:25 +0300

    Добавлен компонент для вывода отделений и докторов

---
+++ front-end/eliz/src/components/DepartmensAndDoctors.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { Row } from 'react-bootstrap'
+import axios from 'axios';
+import Doctors from '../components/DoctorComponent'
+
+class Doctors extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = { departments: [] }
+    }
+
+    componentDidMount() {
+        console.log(this.props.departmentId)
+        axios.get(`https://jsonplaceholder.typicode.com/todos`)
+            .then(res => {
+                const departments = res.data;
+                console.log(departments)
+                this.setState({ departments: departments });
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                {
+                    this.state.departments.map(department =>
+                        <div>
+                            <Row>
+                                <h2>{department.Name}</h2>
+                            </Row>
+                            <Row>
+                                <Doctors departmentId={department.Id} />
+                            </Row>
+                        </div>
+                    )
+                }
+            </div>
+        );
+    }
+}
+export default Doctors;
