058abd57d2fb167f2af05df7def2d45e0853828e
Mekon18 <maksim.grummo@mail.ru>
2020-06-04 21:05:08 +0300

    some change

--- DataAccessLayer/Models/DoctorsAdditionalEducation.cs
+++ DataAccessLayer/Models/DoctorsAdditionalEducation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,19 @@ namespace DataAccessLayer.Models
 	{
 		public int Id { get; set; }
 		public string Name { get; set; }
+		[NotMapped]
+		public string EndingString
+		{
+			get
+			{
+				return Ending.ToString("dd.MM.yyyy");
+			}
+			set
+			{
+				var date = value.Split('.').Select(x => int.Parse(x)).ToArray();
+				Ending = new DateTime(date[2], date[1], date[0]);
+			}
+		}
 		public DateTime Ending { get; set; }
 		public int DoctorId { get; set; }
 	}
